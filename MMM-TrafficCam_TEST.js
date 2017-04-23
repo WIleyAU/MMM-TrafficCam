54a2e72d22138da1300d9666147b32b2fa2114dc
WIleyAU <scott.kemble@hotmail.co.uk>
2017-04-23 23:31:07 +1000

    Add files via upload

--- MMM-TrafficCam_TEST.js
+++ MMM-TrafficCam_TEST.js
@@ -8,138 +8,42 @@
 Module.register('MMM-TrafficCam', {
 
     defaults: {
-        updateInterval: 5*60*1000,
+        updateInterval: 3*60*1000,
         animationSpeed: 0,
         header: '',
-        maxlongedge: 300,
+        imageSize: 400,
         camID: "",
         camRegion: "SYD_MET",
         apiKey: "",
         loadingText: "Loading..."
     },
+    start: function () {
+        self = this;
+        this.url = 'http://www.rms.nsw.gov.au/trafficreports/cameras/camera_images/anzacbr.jpg';
+        setInterval(function () {
+            self.updateDom(1000);
+            console.log('update')
+        }, this.config.updateInterval);
+       
+    },
 
-getScripts: function() {
-    return ["moment.js"];
-},
+    getStyles: function () {
+        return ["trafficcams.css"]
+    },
 
+    // Override dom generator.
 
-//Define start sequence
-start: function () {
-    log.log('Starting module: ' + this.name);
-    this.data.classes = 'bright medium';
-    this.loaded = false;
-    this.camList = {};
-    this.activeItem = 0;
-    this.camURL = "http://www.rms.nsw.gov.au/trafficreports/cameras/camera_images/anzacbr.jpg";
-    //this.sendSocketNotification('CONFIG', this.config);
-    this.testNotificationReceived("CAMERA_IMAGE_LIST", this.camURL);
-},
+    getDom: function () {
+        var wrapper = document.createElement("div");
+        var image = document.createElement("img");
 
-getStyles: function() {
-    return ['trafficcam.css', 'font-awesome.css'];
-},
+        image.src = this.url;
+        image.className = 'MMM-TrafficCam';
 
+        image.width = this.config.imageSize.toString();
+        image.height = this.config.imageSize.toString();
 
-// Override the dom generator
-getDom: function() {
-    var wrapper = document.createElement("div");
-    var imageDisplay = document.createElement('div'); //support for config.changeColor
-
-    if (!this.loaded) {
-        wrapper.innerHTML = this.config.loadingText;
+        wrapper.appendChild(image);
         return wrapper;
     }
-  /*      
-    // set the first item in the list...
-    if (this.activeItem >= this.camList.cams.length) {
-        this.activeItem = 0;
-    }
-        
-    var tempimage = this.camList.cams[this.activeItem];
-       */
-
-
-    // image
-    var imageLink = document.createElement('div');
-    imageLink.id = "MMM-TrafficCam";
-    //imageLink.innerHTML = "<img src='" + tempimage.camURL + "'>";
-    imageLink.innerHTML = "<img src='" + this.camURL + "'>";
-        
-    imageDisplay.appendChild(imageLink);
-    wrapper.appendChild(imageDisplay);
-       
-    return wrapper;
-},
-
-/* scheduleUpdateInterval()
- * Schedule visual update.
- */
-scheduleUpdateInterval: function() {
-    var self = this;
-
-    Log.info("Scheduled update interval set up...");
-    self.updateDom(self.config.animationSpeed);
-
-    setInterval(function() {
-        Log.info("incrementing the activeItem and refreshing");
-        self.activeItem++;
-        self.updateDom(self.config.animationSpeed);
-    }, this.config.updateInterval);
-},
-
-
-// override socketNotificationReceived
-testNotificationReceived: function(notification, payload) {
-    //Log.info('socketNotificationReceived: ' + notification);
-    if (notification === 'CAMERA_IMAGE_LIST')
-    {
-        //Log.info('received CAMERA_IMAGE_LIST');
-        //this.camList = payload;
-            
-        //Log.info("count: " +  this.camList.cams.length);
-            
-        // we want to update the dom the first time and then schedule next updates
-        if (!this.loaded) {
-            this.updateDom(1000);
-            this.scheduleUpdateInterval();
-        }
-            
-        this.loaded = true;
-    }
-}
-
-
-
-
-
-/*
-socketNotificationReceived: function(notification, payload) {
-    log.log('MMM-TraficCams: socketNotificationReceived ' + notification);
-    if (notification === 'CAMERA_IMAGE_LIST') {
-        this.img_src = payload;
-        this.updateDom(this.config.animationSpeed);
-    }
-};
-
-// Override dom generator
-getDom: function () {
-    var wrapper = document.createElement('div');
-    var header = document.createElement("header");
-    var name = document.createElement("span");
-    name.innerHTML = "" + this.config.header;
-    header.appendChild(name);
-    wrapper.appendChild(header);
-
-    var img = document.createElement('img');
-    img.style = "max-width: " + this.config.maxlongedge + "px; max-height: " + this.config.maxlongedge + "px";
-    img.src = this.img_src;
-    img.alt = this.img_src;
-    wrapper.appendChild(img);
-
-    return wrapper;
-},
-
-*/
-
-
 });
