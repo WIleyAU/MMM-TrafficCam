88e3a1b5815a56d04be66b451a7603b82810cc7a
WIleyAU <scott.kemble@hotmail.co.uk>
2017-04-23 15:36:06 +1000

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -35,6 +35,8 @@ module.exports = NodeHelper.create({
         var self = this;
         console.log('retrieveAndUpdate()');
 
+        
+        /* commenting out for testing purposes
         //set https options
         var options = {
             hostname: "api.transport.nsw.gov.au",
@@ -81,5 +83,30 @@ module.exports = NodeHelper.create({
         });
 
         req.end();
+        */
+        var camList = {};
+        camList.cams = new Array ();
+        camList.cams.push({
+            "camID": "d2e649",
+            "camTItle": "Anzac Bridge",
+            "camView": "Intersection of Victoria Road and Anzac Bridge lookingeast towards the Sydney CBD.",
+            "camDirection": "E",
+            "camURL": "http://www.rms.nsw.gov.au/trafficreports/cameras/camera_images/anzacbr.jpg"
+        });
+        camList.cams.push({
+            "camID": "d2e3350",
+            "camTItle": "City West Link",
+            "camView": "City West Link at Lilyfield looking east towards Victoria Road intersection.",
+            "camDirection": "E",
+            "camURL": "http://www.rms.nsw.gov.au/trafficreports/cameras/camera_images/citywestlink.jpg"
+        });
+        camList.cams.push({
+            "camID": "d2e6035",
+            "camTItle": "York Street (Sydney)",
+            "camView": "York Street at Margaret Street looking north towards Sydney Harbour Bridge.",
+            "camDirection": "N",
+            "camURL": "http://www.rms.nsw.gov.au/trafficreports/cameras/camera_images/yorkst_sydney.jpg"
+        });
+        self.sendSocketNotification('CAMERA_IMAGE_LIST', camList);
     }
 });
