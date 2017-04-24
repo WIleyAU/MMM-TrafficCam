1085d296493ab6bbcfd9a3652b7b7946d9daec87
WIleyAU <scott.kemble@hotmail.co.uk>
2017-04-24 15:22:58 +1000

    Add files via upload

--- MMM-TrafficCam.js
+++ MMM-TrafficCam.js
@@ -26,54 +26,7 @@ Module.register('MMM-TrafficCam', {
         this.url = '';
         this.imageList = [];
         this.activeItem = 0;
-        /*
-        this.imageUrls = {
-            'natColor': 'http://www.rms.nsw.gov.au/trafficreports/cameras/camera_images/anzacbr.jpg',
-            'geoColor': 'http://rammb.cira.colostate.edu/ramsdis/online/images/latest/himawari-8/full_disk_ahi_true_color.jpg',
-            'airMass': 'http://rammb.cira.colostate.edu/ramsdis/online/images/latest/himawari-8/full_disk_ahi_rgb_airmass.jpg',
-            'fullBand': 'http://rammb.cira.colostate.edu/ramsdis/online/images/latest/himawari-8/himawari-8_band_03_sector_02.gif',
-            'europeDiscNat': 'http://oiswww.eumetsat.org/IPPS/html/latestImages/EUMETSAT_MSG_RGBNatColour_LowResolution.jpg',
-            'europeDiscSnow': 'http://oiswww.eumetsat.org/IPPS/html/latestImages/EUMETSAT_MSG_RGBSolarDay_CentralEurope.jpg',
-            'centralAmericaDiscNat': 'http://goes.gsfc.nasa.gov/goescolor/goeseast/overview2/color_med/latestfull.jpg'
-
-        }
-        this.hiResImageUrls = {
-            'natColor': 'http://www.rms.nsw.gov.au/trafficreports/cameras/camera_images/harbourbridge.jpg',
-            'geoColor': 'http://rammb.cira.colostate.edu/ramsdis/online/images/latest_hi_res/himawari-8/full_disk_ahi_true_color.jpg',
-            'airMass': 'http://rammb.cira.colostate.edu/ramsdis/online/images/latest_hi_res/himawari-8/full_disk_ahi_rgb_airmass.jpg',
-            'fullBand': 'http://rammb.cira.colostate.edu/ramsdis/online/images/latest/himawari-8/himawari-8_band_03_sector_02.gif',
-            'europeDiscNat': 'http://oiswww.eumetsat.org/IPPS/html/latestImages/EUMETSAT_MSG_RGBNatColour_LowResolution.jpg',
-            'europePartSnow': 'http://oiswww.eumetsat.org/IPPS/html/latestImages/EUMETSAT_MSG_RGBSolarDay_CentralEurope.jpg',
-            'centralAmericaDiscNat': 'http://goes.gsfc.nasa.gov/goescolor/goeseast/overview2/color_lrg/latestfull.jpg'
-        }
-        this.loResImageUrls = {
-            'natColor': 'http://www.rms.nsw.gov.au/trafficreports/cameras/camera_images/yorkst_sydney.jpg',
-            'geoColor': 'http://rammb.cira.colostate.edu/ramsdis/online/images/latest_hi_res/himawari-8/full_disk_ahi_true_color.jpg',
-            'airMass': 'http://rammb.cira.colostate.edu/ramsdis/online/images/latest_hi_res/himawari-8/full_disk_ahi_rgb_airmass.jpg',
-            'fullBand': 'http://rammb.cira.colostate.edu/ramsdis/online/images/latest/himawari-8/himawari-8_band_03_sector_02.gif',
-            'europeDiscNat': 'http://oiswww.eumetsat.org/IPPS/html/latestImages/EUMETSAT_MSG_RGBNatColour_LowResolution.jpg',
-            'europePartSnow': 'http://oiswww.eumetsat.org/IPPS/html/latestImages/EUMETSAT_MSG_RGBSolarDay_CentralEurope.jpg',
-            'centralAmericaDiscNat': 'http://goes.gsfc.nasa.gov/goescolor/goeseast/overview2/color_lrg/latestfull.jpg'
-        }
-        console.log(this.imageUrls[this.config.style]);
-        this.imageList.push(this.imageUrls);
-        this.imageList.push(this.hiResImageUrls);
-        this.imageList.push(this.loResImageUrls);
-        
-        if (this.config.ownImagePath != '') {
-            this.url = this.config.ownImagePath;
-        } else {
-//            if (this.config.imageSize > 800) {
-//                this.url = this.hiResImageUrls[this.config.style];
-//            } else {
-//                this.url = this.imageList[this.activeItem][this.config.style];
-//            }
-            setInterval(function () {
-                self.updateDom(1000);
-                console.log('update')
-            }, this.config.updateInterval);
-        }
-        */
+  
         this.grabCams();
     },
 
@@ -119,7 +72,6 @@ Module.register('MMM-TrafficCam', {
     },
 
     scheduleUpdate: function () {
-        this.activeItem++;
         setInterval(function () {
             self.updateDom(1000);
             console.log('update')
@@ -146,7 +98,7 @@ Module.register('MMM-TrafficCam', {
             this.activeItem = 0;
         }
         this.url = this.imageList[this.activeItem][this.config.style];
-        //        this.activeItem++;
+        this.activeItem++;
 
         var image = document.createElement("img");
         if (this.config.ownImagePath != '') {
