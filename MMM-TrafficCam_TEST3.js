9105cbc8e5c6a05b898f4a68f47f7b5e525a368d
WIleyAU <scott.kemble@hotmail.co.uk>
2017-04-24 16:24:56 +1000

    Add files via upload

--- MMM-TrafficCam_TEST3.js
+++ MMM-TrafficCam_TEST3.js
@@ -6,7 +6,7 @@
  */
 
 var NodeHelper = require('node_helper');
-
+var request = require('request');
 
 
 module.exports = NodeHelper.create({
@@ -23,44 +23,36 @@ module.exports = NodeHelper.create({
         }
     },
 
-    retrieveAndUpdate: function() {
-        var imageUrls = {
-            'natColor': 'http://www.rms.nsw.gov.au/trafficreports/cameras/camera_images/anzacbr.jpg',
-            'geoColor': 'http://rammb.cira.colostate.edu/ramsdis/online/images/latest/himawari-8/full_disk_ahi_true_color.jpg',
-            'airMass': 'http://rammb.cira.colostate.edu/ramsdis/online/images/latest/himawari-8/full_disk_ahi_rgb_airmass.jpg',
-            'fullBand': 'http://rammb.cira.colostate.edu/ramsdis/online/images/latest/himawari-8/himawari-8_band_03_sector_02.gif',
-            'europeDiscNat': 'http://oiswww.eumetsat.org/IPPS/html/latestImages/EUMETSAT_MSG_RGBNatColour_LowResolution.jpg',
-            'europeDiscSnow': 'http://oiswww.eumetsat.org/IPPS/html/latestImages/EUMETSAT_MSG_RGBSolarDay_CentralEurope.jpg',
-            'centralAmericaDiscNat': 'http://goes.gsfc.nasa.gov/goescolor/goeseast/overview2/color_med/latestfull.jpg'
+    retrieveAndUpdate: function () {
+        var self = this;
 
+        var options = {
+            hostname: "api.transport.nsw.gov.au",
+            port: 443,
+            path: "/v1/live/cameras",
+            method: "GET",
+            headers: {
+                "Accept": "application/json",
+                "Authorization": "apikey " + this.config.apiKey
+            }
         };
-        var hiResImageUrls = {
-            'natColor': 'http://www.rms.nsw.gov.au/trafficreports/cameras/camera_images/harbourbridge.jpg',
-            'geoColor': 'http://rammb.cira.colostate.edu/ramsdis/online/images/latest_hi_res/himawari-8/full_disk_ahi_true_color.jpg',
-            'airMass': 'http://rammb.cira.colostate.edu/ramsdis/online/images/latest_hi_res/himawari-8/full_disk_ahi_rgb_airmass.jpg',
-            'fullBand': 'http://rammb.cira.colostate.edu/ramsdis/online/images/latest/himawari-8/himawari-8_band_03_sector_02.gif',
-            'europeDiscNat': 'http://oiswww.eumetsat.org/IPPS/html/latestImages/EUMETSAT_MSG_RGBNatColour_LowResolution.jpg',
-            'europePartSnow': 'http://oiswww.eumetsat.org/IPPS/html/latestImages/EUMETSAT_MSG_RGBSolarDay_CentralEurope.jpg',
-            'centralAmericaDiscNat': 'http://goes.gsfc.nasa.gov/goescolor/goeseast/overview2/color_lrg/latestfull.jpg'
-        };
-        var loResImageUrls = {
-            'natColor': 'http://www.rms.nsw.gov.au/trafficreports/cameras/camera_images/yorkst_sydney.jpg',
-            'geoColor': 'http://rammb.cira.colostate.edu/ramsdis/online/images/latest_hi_res/himawari-8/full_disk_ahi_true_color.jpg',
-            'airMass': 'http://rammb.cira.colostate.edu/ramsdis/online/images/latest_hi_res/himawari-8/full_disk_ahi_rgb_airmass.jpg',
-            'fullBand': 'http://rammb.cira.colostate.edu/ramsdis/online/images/latest/himawari-8/himawari-8_band_03_sector_02.gif',
-            'europeDiscNat': 'http://oiswww.eumetsat.org/IPPS/html/latestImages/EUMETSAT_MSG_RGBNatColour_LowResolution.jpg',
-            'europePartSnow': 'http://oiswww.eumetsat.org/IPPS/html/latestImages/EUMETSAT_MSG_RGBSolarDay_CentralEurope.jpg',
-            'centralAmericaDiscNat': 'http://goes.gsfc.nasa.gov/goescolor/goeseast/overview2/color_lrg/latestfull.jpg'
-        };
-        var imageList = [];
 
-        imageList.push(imageUrls);
-        imageList.push(hiResImageUrls);
-        imageList.push(loResImageUrls);
+        request(options, function(error, response, body)
+        {
+            if (!error && response.statusCode == 200)
+            {
+                var items = JSON.parse(body);
 
-        this.sendSocketNotification("TRAFFIC_CAM_LIST",imageList)
+                var camList = [];
 
-    }
+                items.features.forEach(function (result) {
+                    if (result.properties.region == this.camRegion) {
+                        camList.push(items.features);
+                    }
+                });
+                this.sendSocketNotification("TRAFFIC_CAM_LIST", camList);
+            }
+        });
 
 });
 
