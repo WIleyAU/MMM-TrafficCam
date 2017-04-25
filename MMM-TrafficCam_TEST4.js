9b5105aa2582c96fe27e33203fd74874c89cf130
WIleyAU <scott.kemble@hotmail.co.uk>
2017-04-25 11:32:05 +1000

    Add files via upload

--- MMM-TrafficCam_TEST4.js
+++ MMM-TrafficCam_TEST4.js
@@ -9,8 +9,6 @@ var NodeHelper = require('node_helper');
 var request = require('request');
 var apiKey = "";
 var camRegion = "";
-var callTest = "";
-var imagesWorking = {};
 
 
 module.exports = NodeHelper.create({
@@ -23,22 +21,14 @@ module.exports = NodeHelper.create({
     // Subclass socketNotificationReceived received.
     socketNotificationReceived: function(notification, options) {
         if (notification === 'TRAFFIC_CAM_GET') {
-            console.log("MMM-TrafficCam Notification Received: ", notification);
-            console.log("MMM-TrafficCam apiKey: " + options["apiKey"]);
             this.apiKey = options["apiKey"];
-            console.log("MMM-TrafficCam this.apiKey: " + this.apiKey);
-            console.log("MMM-TrafficCam camRegion: " + options["camRegion"]);
             this.camRegion = options["camRegion"];
-            console.log("MMM-TrafficCam this.camRegion: " + this.camRegion);
-            this.testGrab();
-        } else {
-            this.filterImages();
-        }
+            this.grabCams();
+        } 
     },
     
-    testGrab: function () {
+    grabCams: function () {
         var self = this;
-        console.log('MMM-TrafficCam: testGrab()');
 
         //set https options
         var options = {
@@ -50,51 +40,23 @@ module.exports = NodeHelper.create({
             }
         };
 
-        console.log("MMM-TrafficCam url: " + options["url"]);
-        var reqRegion = this.camRegion;
-        console.log("MMM-TrafficCam REQUEST reqRegion: " + reqRegion);
         request(options, function (error, response, body) {
             if (!error && response.statusCode == 200) {
-                console.log("MMM-TrafficCam statusCode: " + response.statusCode);
-                // get our images out of the INSTAGRAM JSON response
-                this.callTest = "CALL TEST SUCCESS";
-                console.log("MMM-TrafficCam starting call test...");
-                console.log("MMM-TRAFFICCAM CALL TEST: " + this.callTest);
+                // get our images out of the tfNSW JSON response
                 var items = JSON.parse(body);
-                var tempRegion = "";
-                console.log("MMM-TrafficCams IN REQUEST camRegion: " + this.camRegion);
-                console.log("MMM-TrafficCam items: " + items);
+
                 // create our model, a dictionary with
-
                 var images = [];
-                console.log("MMM-TrafficCam new image array length: " + images.length);
                 items.features.forEach(function (results) {
-                    console.log("MMM-TrafficCam forEach start check...");
-                    console.log("MMM-TrafficCam results.properties.region: " + results.properties.region);
-                    tempRegion = results.properties.region;
-                    console.log("MMM-TrafficCam tempRegion: " + tempRegion);
-                    console.log("MMM-TrafficCam IN REQUEST tempRegion(" + tempRegion + ") = camRegion(" + this.camRegion + ")");
                     images.push(results);
-
-                    /*
-                    if (tempRegion == this.camRegion) {
-                        console.log("MMM-TrafficCam this.config.camRegion: " + this.camRegion);
-                        images.push(results);
-                        console.log("MMM-TrafficCam images length working: " + images.length);
-                    };
-                    */
-
                 });
-                console.log("MMM-TrafficCam images length final: " + images.length);
-                console.log("MMM-TrafficCam: Sending Socket Notification");
                 self.sendSocketNotification('TRAFFIC_CAM_LIST', images);
                 //console.log(images)
-
             }
             else {
                 console.log(" Error: " + response.statusCode);
             }
-        }.bind({camRegion:reqRegion}));
+        });
     }
     
 });
